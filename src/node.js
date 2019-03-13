91eada05d8b652e2633ada1558a0dbbd295f118c
esoshyki <esoshyki@yandex.by>
2019-03-13 17:46:22 +0300

    * solve : * write node description *

--- src/node.js
+++ src/node.js
@@ -1,22 +1,34 @@
 class Node {
 	constructor(data, priority) {
+		this.Child = null;
+		this.Parent = null;
+		this.node = [data, priority];
 
 	}
 
 	appendChild(node) {
-
+		this.Child = node;
+		node.Parent = this.node;
 	}
 
 	removeChild(node) {
-
+		this.Child = null;
 	}
 
 	remove() {
-
+		if (this.Parent) {
+			this.Parent.Child = this.Child;
+		}
+		else {
+			this.Child.Parent = null;
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.Parent) {
+			this.Parent.Child = this.Child;
+			this.Child = this.Parent;
+		}	
 	}
 }
 
